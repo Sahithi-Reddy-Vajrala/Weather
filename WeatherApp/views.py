411cf6a9b215c21a238e523d90d9664e2fed6b51
sahithi <sahithireddyvajrala2002@gmail.com>
2021-10-04 20:18:01 +0530

    first commit

---
+++ WeatherApp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+import urllib.request
+import json
+import requests
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=6bc049b14d97f34ee9c5abde29fab429')
+        if response.status_code == 200:
+            # print(city)
+            api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=6bc049b14d97f34ee9c5abde29fab429').read()
+            api_url2 = json.loads(api_url)
+
+            data = {
+                "country": city,
+                "weather_description": api_url2['weather'][0]['description'],
+                "weather_temperature": api_url2['main']['temp'],
+                "weather_pressure": api_url2['main']['pressure'],
+                "weather_humidity":api_url2['main']['humidity'],
+                "weather_icon": api_url2['weather'][0]['icon'],
+            } 
+        else:
+             return render(request,'index.html') 
+    else:
+        city = None
+        data = {
+            "country": None,
+            "weather_description": None,
+            "weather_temperature": None,
+            "weather_pressure": None,
+            "weather_humidity":None,
+            "weather_icon": None,
+        }
+    print(data['weather_icon'])
+    return render(request, 'index.html', {"city": city, "data" :data})
